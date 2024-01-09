8baeb9004fde2debf294bbc03953284c8634de51
정원우 <54103715+klik075@users.noreply.github.com>
2024-01-09 20:14:26 +0900

    게임 시작 함수_map.cs

--- TextRPG/Map.cs
+++ TextRPG/Map.cs
@@ -10,5 +10,23 @@ namespace TextRPG
     internal class Map
     {
 
+        public void StartGame()
+        {
+            while (true) 
+            {
+                Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.");
+                Console.WriteLine("이제 전투를 시작할 수 있습니다.");
+                Console.WriteLine();
+
+                Console.WriteLine("1. 상태 보기");
+                Console.WriteLine("2. 전투 시작");
+                Console.WriteLine();
+
+                Console.WriteLine("원하시는 행동을 입력해주세요.");
+                //Console.Write(">>");
+                Utilities.GetInputKey(1,2, ConsoleColor.White,">>");
+
+            }
+        }
     }
 }
--- TextRPG/Program.cs
+++ TextRPG/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Map map = new();
+            map.StartGame();
             //Map구현 매서드 실행
         }
     }
