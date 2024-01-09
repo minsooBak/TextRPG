5373ce492f178fd3ecb2ec3fc0d1463c39511af1
najdorf <70437537+cn7249@users.noreply.github.com>
2024-01-09 16:42:01 +0900

    기본 아이템 자료 구조 정의
    240109-16:41
    + class Item 생성
    + enum ItemType 생성

--- TextRPG/ItemManager.cs
+++ TextRPG/ItemManager.cs
@@ -9,5 +9,29 @@ namespace TextRPG
     //추민규님 구현
     internal class ItemManager
     {
+        enum ItemType
+        {
+            Weapon,
+            Armor
+        }
+
+        class Item
+        {
+            public string Name = "";
+            public ItemType Type = 0;
+            public int ATK = 0;
+            public int DEF = 0;
+            public string Comment = "";
+
+            // 생성자
+            public Item(string name, int type, int atk, int def, string comment) 
+            { 
+                Name = name;
+                Type = (ItemType)type;
+                ATK = atk;
+                DEF = def;
+                Comment = comment;
+            }
+        }
     }
 }
