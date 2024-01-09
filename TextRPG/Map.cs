efa0e99345e0f81344f336cf9f2582eb4d0d21e4
정원우 <54103715+klik075@users.noreply.github.com>
2024-01-09 20:40:01 +0900

    Update Map.cs

--- TextRPG/Map.cs
+++ TextRPG/Map.cs
@@ -17,26 +17,23 @@ namespace TextRPG
 
         public void StartGame()
         {
-            while (true) 
+            Utilities.AddLine("스파르타 던전에 오신 여러분 환영합니다.");
+            Utilities.AddLine("이제 전투를 시작할 수 있습니다.");
+            Utilities.AddLine("");
+
+            Utilities.AddLine("1. 상태 보기");
+            Utilities.AddLine("2. 전투 시작");
+            Utilities.AddLine("");
+
+            Utilities.AddLine("원하시는 행동을 입력해주세요.");
+            switch ((GameState)Utilities.GetInputKey(1, 2, ConsoleColor.Yellow, ">>"))
             {
-                Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.");
-                Console.WriteLine("이제 전투를 시작할 수 있습니다.");
-                Console.WriteLine();
+                case GameState.PlayerInfo: // 상태 보기
 
-                Console.WriteLine("1. 상태 보기");
-                Console.WriteLine("2. 전투 시작");
-                Console.WriteLine();
+                    break;
+                case GameState.StartBatlle: // 전투 시작
 
-                Console.WriteLine("원하시는 행동을 입력해주세요.");
-                switch ((GameState)Utilities.GetInputKey(1, 2, ConsoleColor.Yellow, ">>"))
-                {
-                    case GameState.PlayerInfo: // 상태 보기
-
-                        break;
-                    case GameState.StartBatlle: // 전투 시작
-
-                        break;
-                }
+                    break;
             }
         }
     }
