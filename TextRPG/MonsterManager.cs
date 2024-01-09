be29bc5bdcfe86081f5b5d8ee6b0df47dd4d78f5
정원우 <54103715+klik075@users.noreply.github.com>
2024-01-09 17:59:43 +0900

    몬스터 주석 추가
    사망했는지 bool값 추가

--- TextRPG/MonsterManager.cs
+++ TextRPG/MonsterManager.cs
@@ -18,15 +18,16 @@ namespace TextRPG
         private static readonly MonsterManager instance = new MonsterManager();
         private MonsterManager() { }
         static public MonsterManager Instance { get { return instance; }}
-        public List<Monster> monsters = new List<Monster>();
+        public List<Monster> monsters = new List<Monster>();//가지고 있는 몬스터 배열 
     }
 
     public class Monster : IListener
     {
-        private string Name { get; set; }
-        private int Lv { get; set; }
-        private int Hp { get; set; }
-        private int Atk { get; set; }
+        private string Name { get; set; } //몬스터 이름
+        private int Lv { get; set; } // 레벨
+        private int Hp { get; set; } // 체력
+        private int Atk { get; set; } //몬스터 공격력
+        public bool isDead; //죽었으면 true
 
         public Monster(MonsterType monsterType = MonsterType.Monster1) //몬스터 초기화
         {
@@ -51,22 +52,23 @@ namespace TextRPG
                 Hp = 25;
                 Atk = 8;
             }
+            isDead = false;
         }
         public void MakeMonster(int num) // num 만큼 몬스터를 생성한다.
         {
-            Random random = new Random(); //랜덤
+            Random random = new Random(); 
             int rnd;
             for (int i = 0; i < num; i++)
             {
-                rnd = random.Next(1,4); // 1 2 3
-                MonsterManager.Instance.monsters.Add(new Monster((MonsterType)rnd)); // 몬스터 생성
+                rnd = random.Next(1,4); // 랜덤 1 2 3
+                MonsterManager.Instance.monsters.Add(new Monster((MonsterType)rnd)); // 몬스터 배열에 저장
             }
         }
         public void OnEvent(EventType type, object? data = null) //이벤트 타입으로 메소드를 실행한다.
         {
             if (type == EventType.eMakeMonster)
             {
-                MakeMonster((int)data);
+                MakeMonster((int)data); //몬스터 생성
             }
         }
     }
