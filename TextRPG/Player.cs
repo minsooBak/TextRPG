86a29ef01a738050bfab1b78276e7053f66fda76
김하챤 <dbtldk12@naver.com>
2024-01-09 20:29:26 +0900

    Update Player.cs

--- TextRPG/Player.cs
+++ TextRPG/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,81 +12,41 @@ namespace TextRPG
 
 
     //유시아님 플레이어 구현 매소드 : 스탯출력, 공격
-    internal class Player : IListener, IObject
+    internal class Player : IListener
     {
-        private ObjectState myState;
-        private int InitATK { get; set; }
-        private int InitDEF { get; set; }
-
-        public Player()
+        private static Character player;
+        static void GameDataSetting()
         {
-            //CreatePlayer
-            CreatePlayer createPlayer = new CreatePlayer();
-            var Name = createPlayer.Create();
-
-            myState.Name = Name.Key;
-            myState.Class = Name.Value;
-
-            myState.Health = 100;
-            myState.MP = 100;
-            myState.Level = 1;
-            myState.EXP = 0;
-            myState.ATK = 10;
-            myState.DEF = 0;
-            myState.Gold = 0;
-            InitATK = myState.ATK;
-            InitDEF = myState.DEF;
+            // 캐릭터 정보 세팅
+            player = new Character("Chad", "전사", 1, 10, 5, 100, 1500);
+            // 아이템 정보 세팅
         }
-
-        public Player(ObjectState state)
-        {
-            myState.Name = state.Name;
-            myState.Class = state.Class;
-            myState.Level = state.Level;
-            myState.ATK = state.ATK;
-            myState.DEF = state.DEF;
-            myState.Health = state.Health;
-            myState.Gold = state.Gold;
-            myState.MP = state.MP;
-
-            InitATK = state.ATK;
-            InitDEF = state.DEF;
-        }
-
-        public int Health => myState.Health;
-        public int MP => myState.MP;
-        public int Level => myState.Level;
-        public string Class => myState.Class;
-        public bool IsDead => myState.Health <= 0;
-        public bool IsUseSkill => myState.Skill.Cost < myState.MP;
-        public void SetSkill(Skill skill) => myState.Skill = skill;
-
         public void OnEvent(EventType type, object data)
         {
             //이벤트 받아서 switch문으로 구현
             
         }
 
-        public int Attack(AttackType attackType)
+        public class Character
         {
-            int damage = 0;
-            double getDamage;
+            public string Name { get; }
+            public string Job { get; }
+            public int Level { get; }
+            public int Atk { get; }
+            public int Def { get; }
+            public int Hp { get; }
+            public int Gold { get; }
 
-            getDamage = myState.ATK / 100.0 * 10;
-            damage = new Random().Next(myState.ATK - (int)Math.Ceiling(getDamage), myState.ATK + (int)Math.Ceiling(getDamage) + 1);
-            if (attackType == AttackType.Skill)
-                damage = myState.Skill.GetATK(damage);
-
-            if (attackType == AttackType.Attack)
-                Console.WriteLine("Chad 의 공격!");
-            else
-                Console.WriteLine($"Chad 의 {myState.Skill.Name} 스킬 공격!");
-            return damage;
-        }
-
-        public void TakeDamage(int damage)
-        {
-            myState.Health -= Math.Clamp(damage, 0 , 100);
+            public Character(string name, string job, int level, int atk, int def, int hp, int gold)
+            {
+                Name = name;
+                Job = job;
+                Level = level;
+                Atk = atk;
+                Def = def;
+                Hp = hp;
+                Gold = gold;
+            }
         }
     }
 }
