30afc800a3cea3ef89d8912e8de36da281f71e4b
minsooBak <minsoo9716@gmail.com>
2024-01-10 12:47:52 +0900

    MonsterSkill추가도 가능하게 변경

--- TextRPG/SkillManager.cs
+++ TextRPG/SkillManager.cs
@@ -13,39 +13,30 @@ namespace TextRPG
     //}
     internal class SkillManager
     {
-        private readonly List<Skill>? skillList = []; //Player의 SkillList
-        public SkillManager(string className)
+        private readonly Skill[] skillList = []; //Player의 SkillList
+        public SkillManager()
         {
-            //스킬데이터 불러오기 후 클래스네임에 따라 다른 스킬배열을 넣어줌
-            //만약 해금하는 식으로 할 경우 SKill에 해금 여부와 조건
-            Skill[] skillList = (Skill[])Utilities.LoadFile(LoadType.SkillData);
-            if(skillList == null)
+            skillList = (Skill[])Utilities.LoadFile(LoadType.SkillData);
+            if (skillList == null)
             {
                 Console.Error.WriteLine("SkillLoad Faill!");
-                return;
             }
-            foreach(Skill skill in skillList)
+        }
+
+        /// <summary>
+        /// 클래스네임에 따라 다른 스킬배열을 넣어줌(PlayerClass, MonsterName)
+        /// </summary>
+        public List<Skill> GetSkills(string className)
+        {
+            List<Skill> list = new List<Skill>();
+            foreach (Skill skill in skillList)
             {
-                if(skill.Class == className)
+                if (skill.Class == className)
                 {
-                    this.skillList.Add(skill);
+                    list.Add(skill);
                 }
             }
-
-        }
-        public Skill[] GetPlayerSkill()
-        {
-            return skillList.ToArray();
-        }
-
-        public Skill GetSkill(int number)
-        {
-            if(number >= skillList.Count)
-            {
-                Console.Error.WriteLine("Skill Get Faill : GetNumber = " + number);
-                return null;
-            }
-            return skillList[number];
+            return list;
         }
     }
 
@@ -54,9 +45,10 @@ namespace TextRPG
        //public SkillType Type { get; private set; } 나중에 패시브 추가할 경우
         public string Name { get; private set; }
         public string Class { get; private set; }//나중에 enum으로도 변경가능
-        public int ATK { get; private set; } // 퍼뎀
+        private float ATK { get; set; } // 퍼뎀
         public int Cost { get; private set; }
         public int CoolDown { get; private set; }
+        public int MaxCoolDown { get; private set; }
         public string Description { get; private set; }
 
         public Skill(string name, string Class, int ATK, int cost, int coolDown, string description)
@@ -66,7 +58,27 @@ namespace TextRPG
             this.ATK = ATK;
             Cost = cost;
             CoolDown = coolDown;
+            MaxCoolDown = coolDown;
             Description = description;
+        }
+
+        public bool isUse()
+        {
+            if (CoolDown == 0) return true;
+            else
+            {
+                CoolDown--;
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 총 피해량을 리턴
+        /// </summary>
+        public int GetATK(int ATK)
+        {
+            CoolDown = MaxCoolDown;
+            return (int)(ATK * this.ATK);
         }
     }
 }
