2a30a2d12669cc7236f34e82addd49c5a44183cc
minsooBak <minsoo9716@gmail.com>
2024-01-10 14:28:54 +0900

    스킬매니저 변경

--- TextRPG/SkillManager.cs
+++ TextRPG/SkillManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,9 +12,11 @@ namespace TextRPG
     //    Active,
     //    Passive
     //}
+
     internal class SkillManager
     {
-        private readonly Skill[] skillList = []; //Player의 SkillList
+        private readonly Skill[] skillList = []; //전체 스킬데이터
+        private Dictionary<string, List<Skill>> skillDictionary = [];//클래스별 스킬데이터
         public SkillManager()
         {
             skillList = (Skill[])Utilities.LoadFile(LoadType.SkillData);
@@ -26,7 +29,7 @@ namespace TextRPG
         /// <summary>
         /// 클래스네임에 따라 다른 스킬배열을 넣어줌(PlayerClass, MonsterName)
         /// </summary>
-        public List<Skill> GetSkills(string className)
+        public void AddSkills(string className)
         {
             List<Skill> list = new List<Skill>();
             foreach (Skill skill in skillList)
@@ -36,7 +39,132 @@ namespace TextRPG
                     list.Add(skill);
                 }
             }
-            return list;
+
+            if(list == null)
+            {
+                Console.Error.WriteLine("SkillsAdd Fail! ClassName : " + className);
+                return;
+            }
+
+            skillDictionary.Add(className, list);
+        }
+
+        public int ShowSkillList(string className)
+        {
+            if (skillDictionary.Count == 0)
+            {
+                Console.Error.WriteLine("Skills Empty");
+                return 0;
+            }
+
+            List<Skill> list;
+            if(skillDictionary.TryGetValue(className, out list) == false)
+            {
+                Console.Error.WriteLine("Skill Show ClassName Null! ClassName : " + className);
+                return 0;
+            }
+
+            for(int i = 0; i < list.Count; i++)
+            {
+                Utilities.AddLine($"{i + 1}. {list[i].Name} - mp {list[i].Cost}");
+                Utilities.AddLine($"   {list[i].Description}.");
+            }
+            return list.Count;
+        }
+
+        /// <summary>
+        /// 몬스터가 스킬을 사용할수 있는지 여부 체크 및 제일 피해량이 높은 스킬 의 피해량과 코스트, number를 배열로 리턴
+        /// / 없다면 null로 리턴
+        /// </summary>
+        public int[] MonsteSkillUse(string className, int ATK, int mp)
+        {
+            if (skillDictionary.Count == 0)
+            {
+                Console.Error.WriteLine("Skills Empty");
+                return null;
+            }
+
+            List<Skill> list;
+            if (skillDictionary.TryGetValue(className, out list) == false)
+            {
+                Console.Error.WriteLine("MonsterSkillUse ClassName Null! ClassName : " + className);
+                return null;
+            }
+
+            Skill? s = null;
+            int max = 0;
+            int count = -1;
+            foreach(Skill skill in list)
+            {
+                if(skill.Cost <= mp)
+                {
+                    if(skill.GetATK(ATK) > max)
+                    {
+                        max = skill.GetATK(ATK);
+                        s = skill;
+                        count++;
+                    }
+                }
+            }
+
+            if(s == null)
+            {
+                return null;
+            }
+
+            return [max, s.Cost, count];
+        }
+
+        /// <summary>
+        /// 플레이어의 스킬 피해량
+        /// </summary>
+        public int GetSkillATK(string className, int number, int ATK)
+        {
+            List<Skill> list;
+            if (skillDictionary.Count == 0)
+            {
+                Console.Error.WriteLine("Skills Empty");
+                return 0;
+            }else if (skillDictionary.TryGetValue(className, out list) == false)
+            {
+                Console.Error.WriteLine($"GetSkillATK Faill! SkillDictionary[{className}] is not Add");
+                return 0;
+            }
+
+            return list[number].GetATK(ATK); 
+        }
+
+        public string GetSkillName(string className, int number)
+        {
+            List<Skill> list;
+            if (skillDictionary.Count == 0)
+            {
+                Console.Error.WriteLine("Skills Empty");
+                return null;
+            }
+            else if (skillDictionary.TryGetValue(className, out list) == false)
+            {
+                Console.Error.WriteLine($"GetSkillATK Faill! SkillDictionary[{className}] is not Add");
+                return null;
+            }
+
+            return list[number].Name;
+        }
+        
+        public int GetCost(string className, int number)
+        {
+            List<Skill> list;
+            if (skillDictionary.Count == 0)
+            {
+                Console.Error.WriteLine("Skills Empty");
+                return 0;
+            }else if(skillDictionary.TryGetValue(className, out list) == false)
+            {
+                Console.Error.WriteLine($"GetSkillATK Faill! SkillDictionary[{className}] is not Add");
+                return 0;
+            }
+
+            return list[number].Cost;
         }
     }
 
@@ -47,29 +175,15 @@ namespace TextRPG
         public string Class { get; private set; }//나중에 enum으로도 변경가능
         private float ATK { get; set; } // 퍼뎀
         public int Cost { get; private set; }
-        public int CoolDown { get; private set; }
-        public int MaxCoolDown { get; private set; }
         public string Description { get; private set; }
 
-        public Skill(string name, string Class, int ATK, int cost, int coolDown, string description)
+        public Skill(string name, string Class, float ATK, int cost, string description)
         {
             Name = name;
             this.Class = Class;
             this.ATK = ATK;
             Cost = cost;
-            CoolDown = coolDown;
-            MaxCoolDown = coolDown;
             Description = description;
-        }
-
-        public bool isUse()
-        {
-            if (CoolDown == 0) return true;
-            else
-            {
-                CoolDown--;
-                return false;
-            }
         }
 
         /// <summary>
@@ -77,7 +191,6 @@ namespace TextRPG
         /// </summary>
         public int GetATK(int ATK)
         {
-            CoolDown = MaxCoolDown;
             return (int)(ATK * this.ATK);
         }
     }
